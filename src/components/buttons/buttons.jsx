2ecb01d8ea4319a59be13774ff56947263d350ef
MV7 <marvin.stoeckemann@web.de>
2025-04-24 12:13:15 +0200

    edit the linkedin and email button style

--- src/components/buttons/buttons.jsx
+++ src/components/buttons/buttons.jsx
@@ -1,10 +1,17 @@
 import './buttons.css';
+import { FaEnvelope, FaLinkedin } from 'react-icons/fa';
 
 export const Button = () => {
   return (
     <div className="button-container">
-      <button className="email">Email</button>
-      <button className="linkedin">Linkedin</button>
+      <button className="email">
+        <FaEnvelope size={16} style={{ marginRight: '8px' }} />
+        Email
+      </button>
+      <button className="linkedin">
+        <FaLinkedin size={16} style={{ marginRight: '8px' }} />
+        Linkedin
+      </button>
     </div>
   );
 };
